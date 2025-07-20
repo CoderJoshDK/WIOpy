d7928dbeff58131bf04b69dd5a505f41429d6e7e
CoderJoshDK <74162303+CoderJoshDK@users.noreply.github.com>
2025-07-20 17:03:34 -0400

    fix: invalid 3.9 syntax with generics

--- wiopy/utils/_cache.py
+++ wiopy/utils/_cache.py
@@ -5,24 +5,26 @@ from __future__ import annotations
 import time
 from functools import lru_cache, update_wrapper
 from math import floor
-from typing import Callable
+from typing import Callable, TypeVar
 
 __all__ = ("ttl_cache",)
 
+_C = TypeVar("_C")
 
-def ttl_cache[C](maxsize: int = 10, typed: bool = False, ttl: int = -1):
+
+def ttl_cache(maxsize: int = 10, typed: bool = False, ttl: int = -1):
     """Generate a TTL cache to be used only for the headers."""
     if ttl <= 0:
         ttl = 65536
 
     hash_gen = _ttl_hash_gen(ttl)
 
-    def wrapper(func: Callable[[C], dict[str, str]]) -> Callable[[C], dict[str, str]]:
+    def wrapper(func: Callable[[_C], dict[str, str]]) -> Callable[[_C], dict[str, str]]:
         @lru_cache(maxsize, typed)
-        def ttl_func(self: C, _ttl_hash: int):
+        def ttl_func(self: _C, _ttl_hash: int):
             return func(self)
 
-        def wrapped(self: C) -> dict[str, str]:
+        def wrapped(self: _C) -> dict[str, str]:
             th = next(hash_gen)
             return ttl_func(self, th)
 
